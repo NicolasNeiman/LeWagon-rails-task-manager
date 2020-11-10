1503ccf0eb279edd302cb7824db91160b7023315
Nicolas Neiman <nicolasneiman@gmail.com>
2020-11-10 13:11:51 +0100

    added create task story

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
   get 'tasks', to: 'tasks#index'
 
   get 'task/:id', to: 'tasks#show', as: 'task'
+
+  get 'tasks/new', to: 'tasks#new'
+  post 'tasks', to: 'tasks#create'
+
 end
