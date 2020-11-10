ccd508b10b95c319f03e9dc3edd5dc1ff2930db5
Nicolas Neiman <nicolasneiman@gmail.com>
2020-11-10 15:07:19 +0100

    refacto route

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'tasks', to: 'tasks#index'
+  # get 'tasks', to: 'tasks#index'
+# 
+  # get 'tasks/new', to: 'tasks#new', as: :new_task
+  # post 'tasks', to: 'tasks#create'
+# 
+  # get 'tasks/:id', to: 'tasks#show', as: :task
+# 
+  # get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  # patch 'tasks/:id', to: 'tasks#update'
+# 
+  # delete 'tasks/:id', to: 'tasks#destroy'
 
-  get 'tasks/edit/:id', to: 'tasks#edit', as: 'task_edit'
-  patch 'task/:id', to: 'tasks#update'
-  
-  get 'task/:id', to: 'tasks#show', as: 'task'
-
-  get 'tasks/new', to: 'tasks#new'
-  post 'tasks', to: 'tasks#create'
-
+  resources :tasks
 end
